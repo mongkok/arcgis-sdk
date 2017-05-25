a100f430b77f1989f04059fc1e449542d6b663e9
mongkok <dani.pyc+github@gmail.com>
2017-05-26 01:56:44 +0200

    add_response function kwargs

--- tests/shortcuts.py
+++ tests/shortcuts.py
@@ -1,17 +1,13 @@
 import arcgis_sdk
-import json
 import responses
 
 
 def add_response(method, path, **kwargs):
     kwargs.setdefault('status', 200)
-
-    if 'body' in kwargs:
-        kwargs['body'] = json.dumps(kwargs['body'])
+    kwargs.setdefault('content_type', 'application/json')
 
     responses.add(
         getattr(responses, method),
         arcgis_sdk.ARCGIS_API_URL + path,
-        content_type='application/json',
         **kwargs
     )
